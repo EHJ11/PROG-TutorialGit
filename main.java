5c63e3fe9dba60db498f248386fa320edea1e722
EHJ11 <usr3468@salesianos-lacuesta.net>
2025-04-25 13:21:06 +0100

    fix: cambio mensaje hola mundo "main.java"

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo y clase");
     }
 }
+/*git status 
+ * git add main.java
+ * git commit -m "clase main creada con hola mundo"
+ * fix: cambio mensaje hola mundo "main.java"
+*/
